88b8ee055b4a37b4a3a0ccdb212d9f7e262d2e1c
Sagramielgo <sagramielgo@gmail.com>
2021-04-12 19:00:08 +0200

    add favicon, change title and background color

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import logo from '../images/s-icon.png';
 import '../stylesSheet/App.scss';
 
 class App extends Component {
@@ -8,8 +8,25 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">¡Hola, mundo!</h1>
+          <h1 className="App-title">My PortFolio</h1>
+          <div className="copy">
+            <a
+              className="firmLink"
+              href="https://github.com/Sagramielgo"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <small className="copy">
+                By #gitFuntastic&nbsp;
+                <span>
+                  <i className="fas fa-paw "></i>
+                </span>
+                &nbsp;2021
+              </small>
+            </a>
+          </div>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
