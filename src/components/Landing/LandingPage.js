8b646a4a4d0080c04f723f93bb4044805a3232b9
Sagramielgo <sagramielgo@gmail.com>
2021-04-13 14:57:58 +0200

    landing page text and structure

--- src/components/Landing/LandingPage.js
+++ src/components/Landing/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 /* import { Link } from 'react-router-dom'; */
-/* import '../stylesheets/_landingPage.scss'; */
+import '../../stylesSheet/landing/landingPage.scss';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -8,11 +8,42 @@ const Landing = () => {
   return (
     <div className="page-landing">
       <Header />
-      <div className="main__button">
-        {/*     <Link to="/card-generator" className="main__button--link">
-          Comenzar
-        </Link> */}
-      </div>
+      <main className="main central-column">
+        <h1 className="main__title"> Sagra Mielgo</h1>
+        {/*  <div className="transparent-container"> */}
+        <article className="article">
+          <ol className="article__list">
+            <li className="article__list--item">
+              Linkedin
+              <i
+                className="article__list--icon fa fa-object-ungroup"
+                aria-hidden="true"
+              ></i>{' '}
+            </li>
+            <li className="article__list--item">
+              Twitter{' '}
+              <i
+                className="article__list--icon fa fa-keyboard-o"
+                aria-hidden="true"
+              ></i>
+            </li>
+            <li className="article__list--item">
+              Youtube
+              <i
+                className="article__list--icon  fa fa-share-alt"
+                aria-hidden="true"
+              ></i>
+            </li>
+          </ol>
+        </article>
+        {/*  </div> */}
+
+        <div className="main__button">
+          {/*  <Link to="/card-generator" className="main__button--link"> */}
+          Proyectos
+          {/*   </Link> */}
+        </div>
+      </main>
       <Footer />
     </div>
   );
