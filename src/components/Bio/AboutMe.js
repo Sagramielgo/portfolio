bcc661eb2627518c6c1c265565f842494b37dd95
Sagramielgo <sagramielgo@gmail.com>
2021-04-14 21:39:39 +0200

    add aboutMe basic styles

--- src/components/Bio/AboutMe.js
+++ src/components/Bio/AboutMe.js
@@ -1,8 +1,27 @@
+import '../../stylesSheet/bio/aboutMe.scss';
 import React from 'react';
 function AboutMe() {
   return (
-    <div className="App">
-      <h1>About Me</h1>
+    <div>
+      <h1 className="titleBio">About Me</h1>
+      <main className="mainContainer">
+        <article className="articleBio articleBio1">
+          <p className="text text1">
+            Lorem Impsum bbbbbbbbbb bbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbb
+            bbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbb
+            bbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbb
+            bbbbbbbb
+          </p>
+        </article>
+        <article className="articleBio articleBio2">
+          <p className="text text2">
+            Lorem Impsum bbbbbbbbbb bbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbb
+            bbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbb
+            bbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbb
+            bbbbbbbb
+          </p>
+        </article>
+      </main>
     </div>
   );
 }
