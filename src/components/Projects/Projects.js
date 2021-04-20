520546b66895fd38b9abb8c3029194b59fc84952
Sagramielgo <sagramielgo@gmail.com>
2021-04-20 22:39:46 +0200

    fix presentations section issues

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,7 +25,7 @@ function Projects() {
             getHideColor={handleRemoveColor}
           />
         </main>
-        <h1 className="main__title"> Video demos</h1>
+        <h1 className="main__title"> Presentations</h1>
 
         <nav className="article videoNav">
           <ul className="article__list">
@@ -44,20 +44,6 @@ function Projects() {
               </a>
             </li>
             <li className="article__list--item">
-              This is Me
-              <a
-                className="article__list--link"
-                href="https://www.youtube.com/watch?v=SfTgr9eAIKc/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <i
-                  className="article__list--icon fas fa-tv"
-                  aria-hidden="true"
-                ></i>
-              </a>
-            </li>
-            <li className="article__list--item">
               React Magicians
               <a
                 className="article__list--link"
@@ -67,6 +53,20 @@ function Projects() {
               >
                 <i
                   className="article__list--icon fas fas fa-tree"
+                  aria-hidden="true"
+                ></i>
+              </a>
+            </li>
+            <li className="article__list--item">
+              This is Me
+              <a
+                className="article__list--link"
+                href="https://www.youtube.com/watch?v=SfTgr9eAIKc/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i
+                  className="article__list--icon fas fa-tv"
                   aria-hidden="true"
                 ></i>
               </a>
